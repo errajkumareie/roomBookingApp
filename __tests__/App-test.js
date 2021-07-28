8632fc4020a1b8502823e7149b4ceabdbbbb48ed
rajkumar.n <rajkumar.natarajan@object-frontier.com>
2021-07-28 12:31:06 +0530

    snapshot-testing

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -4,11 +4,28 @@
 
 import 'react-native';
 import React from 'react';
-import App from '../App';
+import Home from '../src/screens/Home';
+import Header from '../src/components/Header';
+import Counter from '../src/components/Counter';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+jest.mock('@fortawesome/react-native-fontawesome', () => ({
+  FontAwesomeIcon: '',
+}));
+
+it('renders Home Correctly', () => {
+  const home = renderer.create(<Home />).toJSON();
+  expect(home).toMatchSnapshot();
+});
+
+it('renders Header Correctly', () => {
+  const header = renderer.create(<Header />).toJSON();
+  expect(header).toMatchSnapshot();
+});
+
+it('renders Counter Correctly', () => {
+  const counter = renderer.create(<Counter />).toJSON();
+  expect(counter).toMatchSnapshot();
 });
